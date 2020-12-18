3a4614b14e0d679811eb7afc428a2de9e2468a24
kunjuili <k.r.li07717@gmail.com>
2020-12-18 14:09:42 +0800

    fix unexpected rerender when click on addTodoModal

--- src/components/AddTodoModal.tsx
+++ src/components/AddTodoModal.tsx
@@ -5,108 +5,109 @@ import "./Todo.css";
 import { ITodoProps, AddTodoModalProps } from "./interfaces";
 
 const AddTodoModal: React.FunctionComponent<AddTodoModalProps> = ({
-	addNewTodoToArray,
-	SetKeyOfNewTodo,
-	todoKey,
-	ShowAddTodoForm,
+  addNewTodoToArray,
+  SetKeyOfNewTodo,
+  todoKey,
+  ShowAddTodoForm,
 }) => {
-	const newTitleValue = useRef<HTMLInputElement>(null);
-	const newDescriptionValue = useRef<HTMLTextAreaElement>(null);
-	const newDueDate = useRef<HTMLInputElement>(null);
-	const newIsDone = useRef<HTMLInputElement>(null);
-	const language = useContext(LanguageContext);
+  const newTitleValue = useRef<HTMLInputElement>(null);
+  const newDescriptionValue = useRef<HTMLTextAreaElement>(null);
+  const newDueDate = useRef<HTMLInputElement>(null);
+  const newIsDone = useRef<HTMLInputElement>(null);
+  const language = useContext(LanguageContext);
 
-	return (
-		<div styleName="new-todo-form-div">
-			<form>
-				<input
-					role="newTodoTitleInput"
-					placeholder={language.ToDoTitle}
-					type="text"
-					ref={newTitleValue}
-				/>
-				<br />
-				<textarea
-					role="newTodoDescriptionInput"
-					placeholder={language.Description}
-					ref={newDescriptionValue}
-				/>
-				<br />
-				<label>{language.Due}</label>
-				<input role="ChooseDue" type="date" ref={newDueDate} />
-				<br />
-				<input role="isDone" type="checkbox" ref={newIsDone} />
-				<label>{language.Completed}</label>
-				<br />
-				<button
-					type="button"
-					role="addTodo"
-					onClick={() => {
-						SetKeyOfNewTodo();
-						// console.log(`todoKey: ${todoKey}`);
-						let newFormElements: ITodoProps | string;
-						//to avoid 'error: object is possibly null'
-						if (
-							newTitleValue &&
-							newDescriptionValue &&
-							newIsDone &&
-							newDueDate &&
-							newDueDate.current &&
-							newTitleValue.current &&
-							newDescriptionValue.current &&
-							newIsDone.current
-						) {
-							newFormElements = {
-								key: todoKey.toString(),
-								index: todoKey.toString(),
-								caption: newTitleValue.current.value,
-								description: newDescriptionValue.current.value,
-								addedDate: new Date().toLocaleDateString("zh-TW", {
-									year: "numeric",
-									month: "2-digit",
-									day: "2-digit",
-									hour: "numeric",
-									minute: "2-digit",
-								}),
-								due: newDueDate.current.value,
-								isCompleted: Boolean(newIsDone.current.checked),
-							};
-						} else {
-							newFormElements = "Failure building Form. Something is null.";
-						}
-						if (
-							typeof newFormElements !== "string" &&
-							newTitleValue &&
-							newDescriptionValue &&
-							newIsDone &&
-							newTitleValue.current &&
-							newDescriptionValue.current &&
-							newIsDone.current
-						) {
-							addNewTodoToArray(newFormElements);
-							// reset form to empty
-							newTitleValue.current.value = "";
-							description: newDescriptionValue.current.value = "";
-							if (newDueDate && newDueDate.current) {
-								newDueDate.current.value = "";
-							}
-							newIsDone.current.checked = false;
-						}
-					}}
-				>
-					<i className="fas fa-check"></i>
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						ShowAddTodoForm();
-					}}
-				>
-					<i className="fas fa-times"></i>
-				</button>
-			</form>
-		</div>
-	);
+  return (
+    <div styleName="new-todo-form-div">
+      <form>
+        <input
+          role="newTodoTitleInput"
+          placeholder={language.ToDoTitle}
+          type="text"
+          ref={newTitleValue}
+        />
+        <br />
+        <textarea
+          role="newTodoDescriptionInput"
+          placeholder={language.Description}
+          ref={newDescriptionValue}
+        />
+        <br />
+        <label>{language.Due}</label>
+        <input role="ChooseDue" type="date" ref={newDueDate} />
+        <br />
+        <input role="isDone" type="checkbox" ref={newIsDone} />
+        <label>{language.Completed}</label>
+        <br />
+        <button
+          type="button"
+          role="addTodo"
+          onClick={() => {
+            SetKeyOfNewTodo();
+            // console.log(`todoKey: ${todoKey}`);
+            let newFormElements: ITodoProps | string;
+            //to avoid 'error: object is possibly null'
+            if (
+              newTitleValue &&
+              newDescriptionValue &&
+              newIsDone &&
+              newDueDate &&
+              newDueDate.current &&
+              newTitleValue.current &&
+              newDescriptionValue.current &&
+              newIsDone.current
+            ) {
+              newFormElements = {
+                key: todoKey.toString(),
+                index: todoKey.toString(),
+                caption: newTitleValue.current.value,
+                description: newDescriptionValue.current.value,
+                addedDate: new Date().toLocaleDateString("zh-TW", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                  hour: "numeric",
+                  minute: "2-digit",
+                }),
+                due: newDueDate.current.value,
+                isCompleted: Boolean(newIsDone.current.checked),
+              };
+            } else {
+              newFormElements = "Failure building Form. Something is null.";
+            }
+            if (
+              typeof newFormElements !== "string" &&
+              newTitleValue &&
+              newDescriptionValue &&
+              newIsDone &&
+              newTitleValue.current &&
+              newDescriptionValue.current &&
+              newIsDone.current
+            ) {
+              addNewTodoToArray(newFormElements);
+              // reset form to empty
+              newTitleValue.current.value = "";
+              description: newDescriptionValue.current.value = "";
+              if (newDueDate && newDueDate.current) {
+                newDueDate.current.value = "";
+              }
+              newIsDone.current.checked = false;
+            }
+            ShowAddTodoForm();
+          }}
+        >
+          <i className="fas fa-check"></i>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            ShowAddTodoForm();
+          }}
+        >
+          <i className="fas fa-times"></i>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTodoModal;
